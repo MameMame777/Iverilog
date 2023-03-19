bfdf4a52cb61aa8a35b28aefe7559acae6c91642
takumi <takutakuwith@outlook.jp>
2023-03-19 16:08:06 +0900

    add casez dont care

--- RTL/DUV.sv
+++ RTL/DUV.sv
@@ -6,7 +6,7 @@ module DUV(
     output logic [1:0] out_a,
     output logic [1:0] out_b
 );
-
+    logic [1:0] out_c;
     always_ff @(posedge clk or posedge arst) begin
         if(arst==1'b1) begin
             out_a[1:0] <= 2'd0;
@@ -16,4 +16,17 @@ module DUV(
             out_b[1:0] <= {out_b[0],in_b};
         end
     end
+
+    always_ff@(posedge clk or posedge arst)begin
+        if(arst==1'b1) begin
+        end else begin
+            casez(out_a[1:0])  
+                2'b00  :begin out_c[1:0] <= 2'b00; end
+                2'b0?  :begin out_c[1:0] <= 2'b01; end
+                2'b1?  :begin out_c[1:0] <= 2'b11; end
+                default:begin out_c[1:0] <= 2'b00; end
+            endcase
+        end
+    end
+
 endmodule
