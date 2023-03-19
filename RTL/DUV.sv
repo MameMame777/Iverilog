8b171095de59e70c892eb72a47caed2eb80e7410
takumi <takutakuwith@outlook.jp>
2023-03-19 15:06:22 +0900

    initial

---
+++ RTL/DUV.sv
@@ -0,0 +1,19 @@
+module DUV(
+    input logic clk,
+    input logic arst,
+    input logic in_a,
+    input logic in_b,
+    output logic [1:0] out_a,
+    output logic [1:0] out_b
+);
+
+    always @(posedge clk or posedge arst) begin
+        if(arst==1'b1) begin
+            out_a[1:0] <= 2'd0;
+            out_b[1:0] <= 2'd0;
+        end else begin
+            out_a[1:0] <= {out_a[0],in_a};
+            out_b[1:0] <= {out_b[0],in_b};
+        end
+    end
+endmodule
---
+++ SIM/SIM_TOP.sv
@@ -0,0 +1,50 @@
+//
+`timescale 1ns/10ps
+//parameter decralations
+module SIM_TOP;
+
+parameter CLK_CYC = 10;
+logic  clk;
+logic  arst;
+logic  in_a;
+logic  in_b;
+logic [1:0] out_a;
+logic [1:0] out_b;
+
+//instanciate
+DUV u_DUV (
+ .clk  (clk),
+ .arst (arst),
+ .in_a (in_a),
+ .in_b (in_b),
+ .out_a(out_a[1:0]),
+ .out_b(out_b[1:0])
+);
+
+//initial 
+initial clk = 0;
+always begin
+  #(CLK_CYC/2) clk = ~clk;
+end
+
+initial begin
+    in_a=0;
+    in_b=0;
+    @(posedge clk) ;
+        arst <= 1'b1;
+    @(posedge clk) ;
+        arst <= 1'b0;
+    @(posedge clk) ;
+        in_a <=1'b1;
+    @(posedge clk) ;
+        in_b <=1'b1;
+    repeat(20)
+    @(posedge clk) ;
+    $finish;
+end
+
+initial begin
+  $dumpfile("waves.vcd");
+  $dumpvars(0, u_DUV);
+end
+endmodule
